ffc1511de61e19fd37aa908b796722560005819f
benimut2012 <benimutwale@gmail.com>
2018-06-06 18:02:07 +0100

    Spring Project Setup Succesesfully

---
+++ eshopbackend/src/main/java/beni/com/eshopbackend/App.java
@@ -0,0 +1,13 @@
+package beni.com.eshopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
